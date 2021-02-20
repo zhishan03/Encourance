f49704bc1ccf01b58d45aff7bd9c7c9a59a76719
zhishan03 <63858030+zhishan03@users.noreply.github.com>
2021-02-20 18:27:54 -0500

    Co-authored-by: Alyanna Santos <al-yanna@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-#class Todo(db.Model):
-    #id = db.Column(db.Integer, primary_key=True)
-    #content = db.Column(db.Integer, default=0)
-    #date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/tracker")
+def tracker():
+    return render_template('tracker.html')
 
 if __name__ == "__main__":
     app.run()
