263b3493b2ddcc241817e19989ff5508ee01d85c
zhishan03 <63858030+zhishan03@users.noreply.github.com>
2021-02-20 15:45:22 -0500

    styling + pages

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #content = db.Column(db.Integer, default=0)
+    #date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
