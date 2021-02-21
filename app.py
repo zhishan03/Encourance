0ad8fb361119c5aebd559ca5632b9c08cf4ff481
zhishan03 <63858030+zhishan03@users.noreply.github.com>
2021-02-20 23:51:25 -0500

    Co-authored-by: Alyanna Santos <al-yanna@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,9 +13,17 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route("/tracker")
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+@app.route("/tracker", methods=['GET', 'POST'])
 def tracker():
     return render_template('tracker.html')
+
+@app.route("/timer", methods=['GET', 'POST'])
+def timer():
+    return render_template('timer.html')
 
 if __name__ == "__main__":
     app.run()
